a9d9a40507ae31cd4d6a95123f49a3a83e674dd5
EricLBuehler <ericlbuehler@gmail.com>
2023-07-10 18:34:59 -0400

    Update performance graph

--- figures/perfplot.py
+++ figures/perfplot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-plt.plot([1000, 5000, 100000, 500000], [10.945, 12.300, 30.666, 88.387], color = "red")
-plt.plot([1000, 5000, 100000, 500000], [13.754, 18.104, 126.330, 534.67], color = "blue")
+plt.plot([1000, 5000, 100000, 500000], [2.8, 3.48, 18.451, 71.49], color = "red")
+plt.plot([1000, 5000, 100000, 500000], [4.02, 9.415, 137.980, 680.18], color = "blue")
 plt.ylabel("Benchmarked time (ms)")
 plt.xlabel("Number of iterations")
 plt.title("Arc vs Trc performance")
