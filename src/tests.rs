986dcd86e3c2702bbcc76b59a2f9d2dc05b484fb
EricLBuehler <ericlbuehler@gmail.com>
2023-09-02 14:30:53 -0400

    Test examples

--- src/tests.rs
+++ src/tests.rs
@@ -324,3 +324,53 @@ fn test_dispatchfromdyn_sharedtrc() {
     let vehicle: SharedTrc<dyn Vehicle> = shared;
     vehicle.drive();
 }
+
+#[test]
+fn test_ex1() {
+    let mut trc = Trc::new(100);
+    assert_eq!(*trc, 100);
+    *Trc::get_mut(&mut trc).unwrap() = 200;
+    assert_eq!(*trc, 200);
+}
+
+#[test]
+fn test_ex2() {
+    use std::thread;
+
+    let trc = Trc::new(100);
+    let shared = SharedTrc::from_trc(&trc);
+    let handle = thread::spawn(move || {
+        let trc = SharedTrc::to_trc(shared);
+        assert_eq!(*trc, 100);
+    });
+
+    handle.join().unwrap();
+    assert_eq!(*trc, 100);
+}
+
+#[test]
+fn test_ex3() {
+    let trc = Trc::new(100);
+    let weak = Trc::downgrade(&trc);
+    let mut new_trc = weak.upgrade().unwrap();
+    assert_eq!(*new_trc, 100);
+    drop(trc);
+    drop(weak);
+    *Trc::get_mut(&mut new_trc).unwrap() = 200;
+    assert_eq!(*new_trc, 200);
+}
+
+#[test]
+fn test_ex4() {
+    use std::thread;
+    
+    let trc = Trc::new(100);
+    let weak = Trc::downgrade(&trc);
+    
+    let handle = thread::spawn(move || {
+        let trc = weak.upgrade().unwrap();
+        assert_eq!(*trc, 100);
+    });
+    handle.join().unwrap();
+    assert_eq!(*trc, 100);
+}
